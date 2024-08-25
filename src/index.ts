9eb16c1f7510a016f5e96ee2071f1596d1b0875b
Alperzkn <20475676+Alperzkn@users.noreply.github.com>
2024-08-25 15:56:29 +0300

    asd

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
-const firstName = "Alper";
-const favoriteNumber = 6;
+const names = ["vickie", "lena", "helena", "robbie", "sarah"];
 
-function printName(name: string) {
-  console.log(name);
-}
+names.push("midgard");
+//names.push(10)
+//names[1] = 12;
 
-printName(firstName);
+const numbers = [1, 2, 3, 4, 56, 734, 23, 74, 12];
+
+numbers.push(40);
+//numbers.push("hello")
+//number[2] = "alper"
+
+const namesandnumbers = [...names, ...numbers];
+namesandnumbers.push("hello");
+namesandnumbers.push(123112);
+namesandnumbers[0] = 200;
+
+const user = {
+  firstName: "Tracy",
+  lastName: "Jenkins",
+  birthDate: new Date(1964, 2, 1),
+};
+
+user.firstName = "alper";
+console.log(user)
