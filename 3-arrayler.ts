f41776b842166c865d023833db5cfd8913b9e02f
Alperzkn <20475676+Alperzkn@users.noreply.github.com>
2024-08-26 16:49:25 +0300

    array completed

--- 3-arrayler.ts
+++ 3-arrayler.ts
@@ -4,11 +4,8 @@
 // Yaşları tutmak üzere "ages" isminde boş bir array oluşturalım.
 // Tür girmeyi unutmayalım.
 
-
 // Kodu buraya yazalım...
-
-
-
+const ages: number[] = [];
 
 // **********************************************
 // ******************* BÖLÜM 2 ******************
@@ -17,12 +14,8 @@
 // iki boyutlu bir array olmalı. Yani array'in içinde array'ler olacak ve bu
 // array'lerin içinde string'ler olacak.
 
-
 // Kodu buraya yazalım...
-
-
-
-
+const gameBoard: string[][] = [];
 // **********************************************
 // ******************* BÖLÜM 3 ******************
 // **********************************************
@@ -30,11 +23,11 @@
 // isimli iki property'ye sahip olsun. Örnek:
 // {name: "coffee mug", price: 11.50}
 
-
 // Kodu buraya yazalım...
-
-
-
+type Product = {
+  name: string;
+  price: number;
+};
 
 // **********************************************
 // ******************* BÖLÜM 4 ******************
@@ -43,5 +36,16 @@
 // yazdığımız "Product" türünden oluşan bir array alsın ve bu array'in içindeki
 // ürünlerin fiyatlarının toplamını dönsün.
 
-
 // Kodu buraya yazalım...
+const basket: Product[] = [
+  { name: "pencil", price: 11 },
+  { name: "book", price: 5 },
+];
+
+const getTotal = (product: Product) => {
+  let sum: number = 0;
+  product.forEach((a) => (sum += a.price));
+  return sum;
+};
+
+console.log(getTotal(basket));
--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,30 @@
-/* const names = ["vickie", "lena", "helena", "robbie", "sarah"];
-
-names.push("midgard");
-//names.push(10)
-//names[1] = 12;
-
-const numbers = [1, 2, 3, 4, 56, 734, 23, 74, 12];
-
-numbers.push(40);
-//numbers.push("hello")
-//number[2] = "alper"
-
-const namesandnumbers = [...names, ...numbers];
-namesandnumbers.push("hello");
-namesandnumbers.push(123112);
-namesandnumbers[0] = 200;
-
-const user = {
-  firstName: "Tracy",
-  lastName: "Jenkins",
-  birthDate: new Date(1964, 2, 1),
+type Product = {
+  name: string;
+  price: number;
 };
 
-user.firstName = "alper";
-console.log(user) */
-/* 
-let greet = () => {
-  console.log("asd");
+// **********************************************
+// ******************* BÖLÜM 4 ******************
+// **********************************************
+// "getTotal" isminde bir fonksiyon oluşturalım. Bu fonksiyon, bir önceki adımda
+// yazdığımız "Product" türünden oluşan bir array alsın ve bu array'in içindeki
+// ürünlerin fiyatlarının toplamını dönsün.
+
+// Kodu buraya yazalım...
+
+const basket: Product[] = [
+  { name: "pencil", price: 11 },
+  { name: "book", price: 5 },
+];
+
+/* let getTotal = basket.forEach((a) => (sum += a.price));
+console.log(basket);
+console.log(sum); */
+
+const asd = (product: Product) => {
+  let sum: number = 0;
+  product.forEach((a) => (sum += a.price));
+  return sum;
 };
 
-greet = () => {
-  console.log("asdasd");
-};
-
-const sum = (num1: number, num2: number, param3?: number | string) => {
-  console.log(num1 + num2);
-  console.log(param3);
-};
-
-sum(1, 3);
- */
-let leapYear: boolean;
-const isLeapYear = (year: number) => {
-  if (year % 4 == 0) {
-    if (!(year % 400 == 0) && year % 100 == 0) {
-      return (leapYear = false);
-    } else if (year % 400 == 0 && year % 100 == 0) {
-      return (leapYear = true);
-    }
-    return (leapYear = true);
-  } else {
-    return (leapYear = false);
-  }
-};
-
-
-// Örnekler:
-console.log("2024  --->  ", isLeapYear(2024)); //-- > true;
-console.log("2021  --->  ", isLeapYear(2021)); //-- > false;
-console.log("2020  --->  ", isLeapYear(2020)); //-- > true;
-console.log("2019  --->  ", isLeapYear(2019)); //-- > false;
-console.log("2000  --->  ", isLeapYear(2000)); //-- > true;
-console.log("1900  --->  ", isLeapYear(1900)); //-- > false;
-console.log("1700  --->  ", isLeapYear(1700)); //-- > false;
-console.log("1600  --->  ", isLeapYear(1600)); //-- > true;
+console.log(asd(basket));
